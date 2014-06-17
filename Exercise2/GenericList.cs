2ec27485c395af098981b416298a11eb3758674f
Enrico Della Monica <enricodellamonica@gmail.com>
2014-06-17 13:22:06 +0200

    first commit

---
+++ Exercise2/GenericList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2 {
+    class GenericList<T> 
+    {
+        private T First;
+        private T Second;
+
+
+        public  GenericList(T x, T y)
+        {
+            this.First = x;
+            this.Second = y;
+
+        }
+
+        public bool compare() {
+            if (First.Equals(Second))
+            {
+                return true;
+            }
+        else
+            {
+                return false;
+            }
+            }
+        }
+    }
---
+++ Exercise2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2 {
+    class Program {
+        static void Main(string[] args) {
+
+            GenericList<int> gl= new GenericList<int>(1,1);
+            
+            Console.WriteLine(gl.compare());
+            Console.ReadLine();
+        }
+        }
+    }
